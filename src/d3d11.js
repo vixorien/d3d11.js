f42db11882d3bd087c117b2591123363a68bf86a
Chris Cascioli <chriscascioli@gmail.com>
2023-05-09 10:15:35 -0400

    Added VS solution.  Started work on buffers.

--- src/d3d11.js
+++ src/d3d11.js
@@ -1,8 +1,14 @@
 // Errors and such
 // See: https://learn.microsoft.com/en-us/windows/win32/direct3d11/d3d11-graphics-reference-returnvalues
 // See: https://learn.microsoft.com/en-us/windows/win32/direct3ddxgi/dxgi-error
-const S_OK = 0;
-const E_FAIL = 0x80004005;
+
+// Random webgl notes:
+//
+// Drawing
+//  - glDrawElements() --> DrawIndexed()
+//  - glDrawArrays() --> Draw()
+//
+
 
 
 // Need global functions to init API
@@ -12,7 +18,7 @@ function D3D11CreateDevice(canvas) // Canvas acts as the adapter here
 	// Verify and turn on WebGL
 	const gl = canvas.getContext("webgl2");
 	if (gl === null) {
-		return E_FAIL;
+		return false; // TODO: Throw exception?
 	}
 
 	return new ID3D11Device(gl);
@@ -29,13 +35,13 @@ function DXGICreateSwapChain(device)
 }
 
 // Currently UNUSED
-class ComPtr
-{
-	#object = {};
+//class ComPtr
+//{
+//	#object = {};
 
-	Get() { return this.object; }
-	Set(obj) { this.object = obj; }
-}
+//	Get() { return this.object; }
+//	Set(obj) { this.object = obj; }
+//}
 
 class IUnknown
 {
@@ -54,27 +60,6 @@ class IUnknown
 	}
 }
 
-class ID3D11Device extends IUnknown
-{
-	#gl;
-
-	constructor(gl)
-	{
-		super();
-		this.gl = gl;
-	}
-
-	GetAdapter()
-	{
-		return this.gl;
-	}
-
-
-	CreateRenderTargetView(resource)
-	{
-		return new ID3D11RenderTargetView(this, resource);
-	}
-}
 
 class ID3D11DeviceChild extends IUnknown
 {
@@ -92,6 +77,52 @@ class ID3D11DeviceChild extends IUnknown
 	}
 }
 
+
+class ID3D11Device extends IUnknown
+{
+	#gl;
+
+	constructor(gl)
+	{
+		super();
+		this.gl = gl;
+	}
+
+	GetAdapter()
+	{
+		return this.gl;
+	}
+
+	// TODO: Add description
+	CreateRenderTargetView(resource)
+	{
+		return new ID3D11RenderTargetView(this, resource);
+	}
+
+	// TODO: Respect buffer desc, use SubresourceData struct for initial data to match d3d spec
+	// TODO: Ensure array types for initial data? 
+	CreateBuffer(bufferDesc, initialData)
+	{
+		// Grab previous buffer
+		var prevBuffer = this.gl.getParameter(this.gl.ARRAY_BUFFER_BINDING);
+
+		// Create and bind new buffer
+		var buffer = this.gl.createBuffer();
+		this.gl.bindBuffer(this.gl.ARRAY_BUFFER, buffer);
+
+		// Any initial data?
+		if (initialData == null)
+			this.gl.bufferData(this.gl.ARRAY_BUFFER, bufferDesc.ByteWidth, this.gl.STATIC_DRAW); // TODO: Static vs. dynamic?  Check usage in desc?
+		else
+			this.gl.bufferData(this.gl.ARRAY_BUFFER, initialData, this.gl.STATIC_DRAW); // TODO: Verify size vs. description?
+
+		// Restore previous buffer and return new one
+		this.gl.bindBuffer(this.gl.ARRAY_BUFFER, prevBuffer);
+		return buffer;
+	}
+}
+
+
 class ID3D11DeviceContext extends ID3D11DeviceChild
 {
 	#gl;
@@ -106,11 +137,11 @@ class ID3D11DeviceContext extends ID3D11DeviceChild
 	ClearRenderTargetView(rtv, color)
 	{
 		// Grab existing target and RTV
-		var framebuffer = this.gl.getParameter(this.gl.FRAMEBUFFER_BINDING);
+		var prevRT = this.gl.getParameter(this.gl.FRAMEBUFFER_BINDING);
 		var rtvResource = rtv.GetResource();
 
 		// Bind the RTV if necessary
-		if (framebuffer != rtvResource)
+		if (prevRT != rtvResource)
 			this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, rtvResource);
 		
 		// Clear
@@ -118,8 +149,8 @@ class ID3D11DeviceContext extends ID3D11DeviceChild
 		this.gl.clear(this.gl.COLOR_BUFFER_BIT);
 
 		// Reset old framebuffer if necessary
-		if (framebuffer != rtvResource)
-			this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, framebuffer);
+		if (prevRT != rtvResource)
+			this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, prevRT);
 	}
 }
 
@@ -144,18 +175,78 @@ class IDXGISwapChain extends IUnknown
 }
 
 // -----------------------------------------------------
+// ------------------- Descriptions --------------------
+// -----------------------------------------------------
+
+const D3D11_USAGE = {
+	D3D11_USAGE_DEFAULT: 0,
+	D3D11_USAGE_IMMUTABLE: 1,
+	D3D11_USAGE_DYNAMIC: 2,
+	D3D11_USAGE_STAGING: 3
+}
+
+class D3D11_BUFFER_DESC
+{
+	ByteWidth;
+	Usage;
+	BindFlags;
+	CPUAccessFlags;
+	MiscFlags;
+	StructureByteStride;
+
+	constructor(
+		byteWidth,
+		usage,
+		bindFlags,
+		cpuAccessFlags = 0,
+		miscFlags = 0,
+		structureByteStride = 0
+	)
+	{
+		this.ByteWidth = byteWidth;
+		this.Usage = usage;
+		this.BindFlags = bindFlags;
+		this.CPUAccessFlags = cpuAccessFlags;
+		this.MiscFlags = miscFlags;
+		this.StructureByteStride = structureByteStride;
+	}
+}
+
+
+
+// -----------------------------------------------------
 // --------------------- Resources ---------------------
 // -----------------------------------------------------
 
 class ID3D11Resource extends ID3D11DeviceChild
 {
+	#desc;
 
+	constructor(device, description)
+	{
+		super(device);
+		this.desc = description;
+	}
+
+	GetDesc()
+	{
+		return desc;
+	}
+}
+
+class ID3D11Buffer extends ID3D11Resource
+{
+	constructor(device, description)
+	{
+		super(device, description);
+	}
 }
 
 class ID3D11Texture2D extends ID3D11Resource
 {
 
 }
+
 
 // -----------------------------------------------------
 // ----------------------- Views -----------------------
