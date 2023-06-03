695301927da0d5b8a46d05acd8a762e19046f545
Chris Cascioli <chriscascioli@gmail.com>
2023-06-03 10:14:34 -0400

    Playing around with an input manager

---
+++ src/input.js
@@ -0,0 +1,31 @@
+
+class Input
+{
+	#keyStates;
+
+	// For removal
+	#boundKeyDownHandler;
+	#boundKeyUpHandler;
+
+	constructor(element)
+	{
+		this.#keyStates = [];
+
+		element.addEventListener("keydown", this.#boundKeyDownHandler = this.#KeyDownHandler.bind(this));
+		element.addEventListener("keyup", this.#boundKeyUpHandler = this.#KeyUpHandler.bind(this));
+		
+
+	}
+
+	
+	#KeyDownHandler(e)
+	{
+		this.#keyStates[e.location] = true;
+		console.log(e);
+	}
+
+	#KeyUpHandler(e)
+	{
+		this.#keyStates[e.location] = false;
+	}
+}
