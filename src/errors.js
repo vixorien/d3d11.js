a01a28b27ab89a7cdc8aee248e4cd16791354f69
Chris Cascioli <chriscascioli@gmail.com>
2023-06-08 17:49:17 -0400

    Added errors.js file to consolidate error messages

---
+++ src/errors.js
@@ -0,0 +1,15 @@
+﻿
+// -----------------------------------------------------
+// -------------- D3D11 & d3d11.js Errors --------------
+// -----------------------------------------------------
+
+// --- Example Errors ---
+//
+// 1. Invalid array size on texture2d creation:
+//
+// D3D11 ERROR: ID3D11Device::CreateTexture2D: The Dimensions are invalid. For feature level D3D_FEATURE_LEVEL_11_0, the Width (value = 256) must be between 1 and 16384, inclusively. The Height (value = 256) must be between 1 and 16384, inclusively. And, the ArraySize (value = 0) must be between 1 and 2048, inclusively. [ STATE_CREATION ERROR #101: CREATETEXTURE2D_INVALIDDIMENSIONS]
+//
+// 2. Invalid mip levels on tex2d creation:
+//
+// D3D11 ERROR: ID3D11Device::CreateTexture2D: MipLevels invalid. With the dimensions of Width = 256, Height = 256, and ArraySize = 1, MipLevels (value = 999) must be between 1 and 9, inclusively. [ STATE_CREATION ERROR #102: CREATETEXTURE2D_INVALIDMIPLEVELS]
+//
